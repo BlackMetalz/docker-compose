d0f05bc4ada9f4f15235c30d205ac56e37cd9c4b
Luong Trung Kien <kienlt.qn@gmail.com>
2025-01-19 22:50:18 +0700

    improve code test

--- postgresql_patroni_haproxy/resources/code_test/main.go
+++ postgresql_patroni_haproxy/resources/code_test/main.go
@@ -19,37 +19,58 @@ const (
 )
 
 func main() {
-    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-        host, port, user, password, dbname)
+    var db *sql.DB
+    var err error
 
-    db, err := sql.Open("postgres", psqlInfo)
-    if err != nil {
-        log.Fatalf("Unable to connect to database: %v\n", err)
+    for {
+        psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+            host, port, user, password, dbname)
+
+        db, err = sql.Open("postgres", psqlInfo)
+        if err != nil {
+            log.Printf("Unable to connect to database: %v\n", err)
+            time.Sleep(5 * time.Second)
+            continue
+        }
+
+        ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+        defer cancel()
+
+        err = db.PingContext(ctx)
+        if err != nil {
+            log.Printf("Unable to connect to database: %v\n", err)
+            time.Sleep(5 * time.Second)
+            continue
+        }
+
+        break
     }
     defer db.Close()
 
-    ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-    defer cancel()
+	for {
+        ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 
-    var isReplica bool
-    err = db.QueryRowContext(ctx, "SELECT pg_is_in_recovery()").Scan(&isReplica)
-    if err != nil {
-        log.Fatalf("Query failed: %v\n", err)
-    }
+        var isReplica bool
+        err = db.QueryRowContext(ctx, "SELECT pg_is_in_recovery()").Scan(&isReplica)
+        if err != nil {
+            log.Printf("Query failed: %v\n", err)
+            cancel()
+            time.Sleep(5 * time.Second)
+            continue
+        }
 
-    for {
         if isReplica {
             fmt.Println("Current server is a replica.")
         } else {
             fmt.Println("Current server is the primary.")
         }
 
-        ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-        defer cancel()
-
         rows, err := db.QueryContext(ctx, "SELECT id, name, position, salary, hire_date FROM sample_data")
         if err != nil {
-            log.Fatalf("Query failed: %v\n", err)
+            log.Printf("Query failed: %v\n", err)
+            cancel()
+            time.Sleep(5 * time.Second)
+            continue
         }
         defer rows.Close()
 
@@ -62,11 +83,13 @@ func main() {
 
             err := rows.Scan(&id, &name, &position, &salary, &hireDate)
             if err != nil {
-                log.Fatalf("Row scan failed: %v\n", err)
+                log.Printf("Row scan failed: %v\n", err)
+                continue
             }
             fmt.Printf("%d | %s | %s | %.2f | %s\n", id, name, position, salary, hireDate.Format("2006-01-02"))
         }
 
+        cancel()
         time.Sleep(5 * time.Second)
     }
 }
